6f9a8d6f69c834d3fa157474e54344d462128d7b
chengfangang <chengfangang@qq.com>
2022-02-05 20:01:55 +0800

    add dbuser functions details

---
+++ admin/server/service/dbuser.go
@@ -0,0 +1,58 @@
+package service
+
+import "admin/server/models"
+
+type DBUser struct {
+	ID uint
+
+	Name     string
+	Username string
+	Password string
+
+	Remark string
+
+	Page     int
+	PageSize int
+
+	Dbs []uint
+}
+
+func (d *DBUser) Get() (*models.DBUser, error) {
+	dbUser, err := models.GetDBUser(d.ID)
+	if err != nil {
+		return nil, err
+	}
+	return dbUser, nil
+}
+
+func (d *DBUser) GetList() ([]*models.DBUser, uint, error) {
+	query := make(map[string]interface{})
+	query["name"] = d.Name
+
+	pageSize := d.PageSize
+	page := d.Page
+
+	return models.GetDBUsers(query, page, pageSize)
+}
+
+func (d *DBUser) Save() error {
+	data := map[string]interface{}{
+		"name":     d.Name,
+		"username": d.Username,
+		"password": d.Password,
+		"remark":   d.Remark,
+	}
+
+	var err error
+	if d.ID > 0 {
+		err = models.UpdateDBUser(d.ID, data)
+	} else {
+		err = models.AddDBUser(data)
+	}
+
+	return err
+}
+
+func (d *DBUser) Delete() error {
+	return models.DeleteDBUser(d.ID)
+}
