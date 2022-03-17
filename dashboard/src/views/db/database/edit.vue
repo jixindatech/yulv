0560fba4da83c165db69477eaeab19e920495889
chengfangang <chengfangang@qq.com>
2022-03-17 18:46:41 +0800

    add collation chioce

--- dashboard/src/views/db/database/edit.vue
+++ dashboard/src/views/db/database/edit.vue
@@ -30,13 +30,13 @@
         <el-input v-model.number="formData.port" />
       </el-form-item>
       <el-form-item label="字符集" prop="character">
-        <el-select v-model="formData.charset" placeholder="请选择字符集">
+        <el-select v-model="formData.charset" placeholder="请选择字符集" @change="charsetChanged">
           <el-option v-for="(item,index) in SQL_CHARSET" :key="index" :label="item.label" :value="item.value" />
         </el-select>
       </el-form-item>
       <el-form-item label="字符序" prop="collation">
         <el-select v-model="formData.collation" placeholder="请选择字符序">
-          <el-option v-for="(item,index) in SQL_CHARSET" :key="index" :label="item.label" :value="item.value" />
+          <el-option v-for="(item,index) in collation" :key="index" :label="item.label" :value="item.value" />
         </el-select>
       </el-form-item>
       <el-form-item label="备注：" prop="remark">
@@ -122,6 +122,7 @@ export default {
   data() {
     return {
       SQL_CHARSET,
+      collation: [],
       SQL_COLLATION_BIG5,
       SQL_COLLATION_DEC8,
       SQL_COLLATION_CP850,
@@ -203,6 +204,89 @@ export default {
     handleClose() {
       this.$refs['formData'].resetFields()
       this.remoteClose()
+    },
+    charsetChanged(value) {
+      if (value === 'big5') {
+        this.collation = SQL_COLLATION_BIG5
+      } else if (value === 'dec8') {
+        this.collation = SQL_COLLATION_DEC8
+      } else if (value === 'cp850') {
+        this.collation = SQL_COLLATION_CP850
+      } else if (value === 'hp8') {
+        this.collation = SQL_COLLATION_HP8
+      } else if (value === 'koi8r') {
+        this.collation = SQL_COLLATION_KOI8R
+      } else if (value === 'latin1') {
+        this.collation = SQL_COLLATION_LATIN1
+      } else if (value === 'latin2') {
+        this.collation = SQL_COLLATION_LATIN2
+      } else if (value === 'swe7') {
+        this.collation = SQL_COLLATION_SWE7
+      } else if (value === 'ascii') {
+        this.collation = SQL_COLLATION_ASCII
+      } else if (value === 'ujis') {
+        this.collation = SQL_COLLATION_UJIS
+      } else if (value === 'sjis') {
+        this.collation = SQL_COLLATION_SJIS
+      } else if (value === 'hebrew') {
+        this.collation = SQL_COLLATION_HEBREW
+      } else if (value === 'tis620') {
+        this.collation = SQL_COLLATION_TIS620
+      } else if (value === 'euckr') {
+        this.collation = SQL_COLLATION_EUCKR
+      } else if (value === 'koi8u') {
+        this.collation = SQL_COLLATION_KOI8U
+      } else if (value === 'gb2312') {
+        this.collation = SQL_COLLATION_GB2312
+      } else if (value === 'greek') {
+        this.collation = SQL_COLLATION_GREEK
+      } else if (value === 'cp1250') {
+        this.collation = SQL_COLLATION_CP1250
+      } else if (value === 'gbk') {
+        this.collation = SQL_COLLATION_GBK
+      } else if (value === 'latin5') {
+        this.collation = SQL_COLLATION_LATIN5
+      } else if (value === 'armscii8') {
+        this.collation = SQL_COLLATION_ARMSCII8
+      } else if (value === 'utf8') {
+        this.collation = SQL_COLLATION_UTF8
+      } else if (value === 'ucs2') {
+        this.collation = SQL_COLLATION_UCS2
+      } else if (value === 'cp866') {
+        this.collation = SQL_COLLATION_CP886
+      } else if (value === 'keybcs2') {
+        this.collation = SQL_COLLATION_KEYBCS2
+      } else if (value === 'macce') {
+        this.collation = SQL_COLLATION_MACCE
+      } else if (value === 'cp852') {
+        this.collation = SQL_COLLATION_CP852
+      } else if (value === 'latin7') {
+        this.collation = SQL_COLLATION_LATIN7
+      } else if (value === 'utf8mb4') {
+        this.collation = SQL_COLLATION_UTF8MB4
+      } else if (value === 'cp1251') {
+        this.collation = SQL_COLLATION_CP1251
+      } else if (value === 'utf16') {
+        this.collation = SQL_COLLATION_UTF16
+      } else if (value === 'utf16le') {
+        this.collation = SQL_COLLATION_UTF16LE
+      } else if (value === 'cp1256') {
+        this.collation = SQL_COLLATION_CP1251
+      } else if (value === 'cp1257') {
+        this.collation = SQL_COLLATION_CP1257
+      } else if (value === 'utf32') {
+        this.collation = SQL_COLLATION_UTF32
+      } else if (value === 'binary') {
+        this.collation = SQL_COLLATION_BINARAY
+      } else if (value === 'geostd8') {
+        this.collation = SQL_COLLATION_GEOSTD8
+      } else if (value === 'cp932') {
+        this.collation = SQL_COLLATION_CP932
+      } else if (value === 'eucjpms') {
+        this.collation = SQL_COLLATION_EUCJPMS
+      } else {
+        this.collation = []
+      }
     }
   }
 }
